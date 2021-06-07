bfc3442a6b29cec3d236e2257974f45b55e44fc6
Stickerleee <lgy060@gmail.com>
2021-06-07 18:22:42 +0800

    模拟器布局

--- pages/simulator/simulator.js
+++ pages/simulator/simulator.js
@@ -5,7 +5,66 @@ Page({
      * 页面的初始数据
      */
     data: {
+        soul:[],
+        weapon:[{
+            "idx": 2,
+            "id":"60337",
+            "type": "weapon",
+            name: "山河社稷图",
+            "property": "水",
+            "divinatory": "坎",
+            "icon": "/image/weapon/60305.png",
+            "effect": ["水攻水防+5","对敌人施加的负面状态的持续时间+20%"],
+            "effectUpdata":["",""],
+            "tags":["增益"],
+            "lockEffect":["风攻风防+","水伤加成+","雷攻雷防+","风伤加成+"],
+            "baguaIcon": "/image/bagua/icon/bagua_kan.png",
+            },{}
+        ],
+        shadowpositiv:[],
+        shadownegativ:[],
+        effect:[],
+        showDetail:false,
+        test:"123",
+        tabs:[
+          {
+            "title": "攻击",
+            "title1": "乾",
+          },
+          {
+            "title": "防御",
+            "title1": "坤"
+          },
+          {
+            "title": "火系",
+            "title1": "离"
+          },
+          {
+            "title": "水系",
+            "title1": "坎"
+          },
+          {
+            "title": "雷系",
+            "title1": "震"
+          },
+          {
+            "title": "风系",
+            "title1": "巽"
+          },
+          {
+            "title": "土系",
+            "title1": "艮"
+          },
+          {
+            "title": "治愈",
+            "title1": "兑"
+          }]
+    },
 
+    showDetailStatus(){
+        this.setData({
+            showDetail:!this.data.showDetail
+        })
     },
 
     /**
